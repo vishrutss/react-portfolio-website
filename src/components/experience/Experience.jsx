e8fd6729c626a3b011c3d1d4c1f4b03314a862c8
vishrutss <vishrutss@gmail.com>
2023-07-22 17:50:15 +0530

    Updated experience.jsx

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import './experience.css'
-import { BsFillPatchCheckFill } from 'react-icons/bs'
 
 const Experience = () => {
     return (
@@ -11,9 +10,8 @@ const Experience = () => {
                     <h3>My Experience</h3>
                     <div className="content">
                         <article className='details'>
-                            <BsFillPatchCheckFill className='details-icon' />
                             <div>
-                                <h3>Application Development Analyst</h3>
+                                <h4>Application Development Analyst</h4>
                                 <h5>Accenture Solutions Ltd., Bengaluru, India | Oct 2020 - Jul 2022</h5>
                                 <p>
                                     I worked as part of the Shop Disney Test Automoation team. My work included building and refactoring automated test cases using Node JS and the Cucumber framework. I was also in charge of maintaining Shop Disney Japan's Test Automation Repository. I also was a part of automated test regressions for ShopDisney US, ShopDisney Japan, ShopDisney EMEA, and ShopDisney OMS (Order management system).
@@ -21,9 +19,8 @@ const Experience = () => {
                             </div>
                         </article>
                         <article className='details'>
-                            <BsFillPatchCheckFill className='details-icon' />
                             <div>
-                                <h3>Software Engineering Intern</h3>
+                                <h4>Software Engineering Intern</h4>
                                 <h5>Integra Micro Systems, Bengaluru, India | Jun 2019 - Jul 2019</h5>
                                 <p>
                                     I Created an application which accesses Twitter using the REST API to be able to read the user’s timeline, search for tweets and send tweets. Learned the basics of implementing the functions in the REST API. Completed all aspects of the project including requirements specification, design, implementation, testing and documentation.
@@ -36,22 +33,20 @@ const Experience = () => {
                     <h3>My Education</h3>
                     <div className="content">
                         <article className='details'>
-                            <BsFillPatchCheckFill className='details-icon' />
                             <div>
-                                <h3>Masters In Computer Science</h3>
+                                <h4>Masters In Computer Science</h4>
                                 <h5>Portland State University, Portland, OR, USA | 2022 - present</h5>
-                                <h4>GPA 3.82</h4>
+                                <h5>GPA 3.82</h5>
                                 <p>
                                     I am currently pursuing my Masters Degree at Portland State University. Here are some of the courses I have completed: Programming in RUST, Intro to Database Systems, Machine Learning, Artificial Intelligence, Web & Cloud Systems.
                                 </p>
                             </div>
                         </article>
                         <article className='details'>
-                            <BsFillPatchCheckFill className='details-icon' />
                             <div>
-                                <h3>Bachelors in Computer Science</h3>
+                                <h4>Bachelors in Computer Science</h4>
                                 <h5>NMAM Institute of Technology, Nitte, KA, India | 2016 - 2020</h5>
-                                <h4>CGPA 8.53 out of 10</h4>
+                                <h5>CGPA 8.53 out of 10</h5>
                                 <p>
                                     I am currently pursuing my Masters Degree at Portland State University. Here are some of the courses I have completed: Programming in RUST, Intro to Database Systems, Machine Learning, Artificial Intelligence, Web & Cloud Systems.
                                 </p>
