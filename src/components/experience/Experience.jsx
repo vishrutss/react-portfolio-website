3e7997d26d23e771180157316204321ac44a1b0c
vishrutss <vishrutss@gmail.com>
2023-07-22 18:15:56 +0530

    Minor fixes in experience.jsx

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -12,7 +12,7 @@ const Experience = () => {
                         <article className='experience__details'>
                             <div>
                                 <h4>Application Development Analyst</h4>
-                                <h5>Accenture Solutions Ltd., Bengaluru, India | Oct 2020 - Jul 2022</h5>
+                                <h5>Accenture Solutions Ltd., Bengaluru, India <br />Oct 2020 - Jul 2022</h5>
                                 <p>
                                     I worked as part of the Shop Disney Test Automoation team. My work included building and refactoring automated test cases using Node JS and the Cucumber framework. I was also in charge of maintaining Shop Disney Japan's Test Automation Repository. I also was a part of automated test regressions for ShopDisney US, ShopDisney Japan, ShopDisney EMEA, and ShopDisney OMS (Order management system).
                                 </p>
@@ -21,7 +21,7 @@ const Experience = () => {
                         <article className='experience__details'>
                             <div>
                                 <h4>Software Engineering Intern</h4>
-                                <h5>Integra Micro Systems, Bengaluru, India | Jun 2019 - Jul 2019</h5>
+                                <h5>Integra Micro Systems, Bengaluru, India <br />Jun 2019 - Jul 2019</h5>
                                 <p>
                                     I Created an application which accesses Twitter using the REST API to be able to read the user’s timeline, search for tweets and send tweets. Learned the basics of implementing the functions in the REST API. Completed all aspects of the project including requirements specification, design, implementation, testing and documentation.
                                 </p>
@@ -35,7 +35,7 @@ const Experience = () => {
                         <article className='experience__details'>
                             <div>
                                 <h4>Masters In Computer Science</h4>
-                                <h5>Portland State University, Portland, OR, USA | 2022 - present</h5>
+                                <h5>Portland State University, Portland, OR, USA  <br />2022 - present</h5>
                                 <h5>GPA 3.82</h5>
                                 <p>
                                     I am currently pursuing my Masters Degree at Portland State University. Here are some of the courses I have completed: Programming in RUST, Intro to Database Systems, Machine Learning, Artificial Intelligence, Web & Cloud Systems.
@@ -45,7 +45,7 @@ const Experience = () => {
                         <article className='experience__details'>
                             <div>
                                 <h4>Bachelors in Computer Science</h4>
-                                <h5>NMAM Institute of Technology, Nitte, KA, India | 2016 - 2020</h5>
+                                <h5>NMAM Institute of Technology, Nitte, KA, India  <br />2016 - 2020</h5>
                                 <h5>CGPA 8.53 out of 10</h5>
                                 <p>
                                     I am currently pursuing my Masters Degree at Portland State University. Here are some of the courses I have completed: Programming in RUST, Intro to Database Systems, Machine Learning, Artificial Intelligence, Web & Cloud Systems.
