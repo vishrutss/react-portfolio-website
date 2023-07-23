71c285f4f647258872d1c63df02eb00046ae1b46
vishrutss <vishrutss@gmail.com>
2023-07-23 17:49:25 +0530

    Some fixes in projects.jsx

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -11,68 +11,68 @@ const Projects = () => {
         <section id="projects">
             <h5>My Work</h5>
             <h2>Some of my Projects</h2>
-            <div className="container projects__container">
-                <article className="project__details">
-                    <h3>A Text Based Adventure Game written in RUST</h3>
+            <div className="container project__container">
+                <article className="project__item">
                     <div className="project__item-image">
                         <img src={RUST_IMG} alt="" />
                     </div>
+                    <h3>A Text Based Adventure Game written in RUST</h3>
                     <a href="https://github.com/vishrutss/Text-based-adventure-game-in-Rust" className='btn'>Github</a>
                 </article>
-                <article className="project__details">
-                    <h3>An Amusement Park created using OpenGL</h3>
+                <article className="project__item">
                     <div className="project__item-image">
                         <img src={CPP_IMG} alt="" />
                     </div>
+                    <h3>An Amusement Park created using OpenGL</h3>
                     <a href="https://github.com/vishrutss/Amusement_Park" className='btn'>Github</a>
                 </article>
-                <article className="project__details">
-                    <h3>Image Editor Program</h3>
+                <article className="project__item">
                     <div className="project__item-image">
                         <img src={CPP_IMG} alt="" />
                     </div>
+                    <h3>Image Editor Program</h3>
                     <a href="https://github.com/vishrutss/Image_Editor" className='btn'>Github</a>
                 </article>
-                <article className="project__details">
-                    <h3>Chess AI Project</h3>
+                <article className="project__item">
                     <div className="project__item-image">
                         <img src={PYTHON_IMG} alt="" />
                     </div>
+                    <h3>Chess AI Project</h3>
                     <a href="https://github.com/vishrutss/ChessProject" className='btn'>Github</a>
                 </article>
-                <article className="project__details">
-                    <h3>A group of JavaScript Projects</h3>
+                <article className="project__item">
                     <div className="project__item-image">
                         <img src={JS_IMG} alt="" />
                     </div>
+                    <h3>A group of JavaScript Projects</h3>
                     <a href="https://github.com/vishrutss/Learning-JavaScript" className='btn'>Github</a>
                 </article>
-                <article className="project__details">
-                    <h3>Vulgar tweet identification using Machine Learning</h3>
+                <article className="project__item">
                     <div className="project__item-image">
                         <img src={PYTHON_IMG} alt="" />
                     </div>
+                    <h3>Vulgar tweet identification using Machine Learning</h3>
                     <a href="https://github.com/queoui/VulgarTweetIdentification" className='btn'>Github</a>
                 </article>
-                <article className="project__details">
-                    <h3>Internet Relay Chat Project</h3>
+                <article className="project__item">
                     <div className="project__item-image">
                         <img src={PYTHON_IMG} alt="" />
                     </div>
+                    <h3>Internet Relay Chat Project</h3>
                     <a href="https://github.com/master-of-none/IPProject" className='btn'>Github</a>
                 </article>
-                <article className="project__details">
-                    <h3>Product Key Activation using Asymmetric Cryptography</h3>
+                <article className="project__item">
                     <div className="project__item-image">
                         <img src={PYTHON_IMG} alt="" />
                     </div>
+                    <h3>Product Key Activation using Asymmetric Cryptography</h3>
                     <a href="https://github.com/vishrutss/Product_Key_Activation_Project" className='btn'>Github</a>
                 </article>
-                <article className="project__details">
-                    <h3>Driver Drowsiness Detection program</h3>
+                <article className="project__item">
                     <div className="project__item-image">
                         <img src={MATLAB_IMG} alt="" />
                     </div>
+                    <h3>Driver Drowsiness Detection program</h3>
                     <a href="https://github.com/vishrutss/Driver-Drowsiness-Detection" className='btn'>Github</a>
                 </article>
             </div>
