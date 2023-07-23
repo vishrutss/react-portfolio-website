91888bf8cb5602450554015258691267eabf1975
vishrutss <vishrutss@gmail.com>
2023-07-23 18:05:58 +0530

    Implemented arrays in projects.jsx

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -6,75 +6,82 @@ import PYTHON_IMG from '../../assets/python.png'
 import JS_IMG from '../../assets/js.png'
 import MATLAB_IMG from '../../assets/matlab.png'
 
+const data = [
+    {
+        id: 1,
+        title: 'A Text Based Adventure Game written in RUST',
+        img: RUST_IMG,
+        github: 'https://github.com/vishrutss/Text-based-adventure-game-in-Rust'
+    },
+    {
+        id: 2,
+        title: 'An Amusement Park created using OpenGL',
+        img: CPP_IMG,
+        github: 'https://github.com/vishrutss/Amusement_Park'
+    },
+    {
+        id: 3,
+        title: 'Image Editor Program',
+        img: CPP_IMG,
+        github: 'https://github.com/vishrutss/Image_Editor'
+    },
+    {
+        id: 4,
+        title: 'Chess AI Project',
+        img: PYTHON_IMG,
+        github: 'https://github.com/vishrutss/ChessProject'
+    },
+    {
+        id: 5,
+        title: 'A group of JavaScript Projects',
+        img: JS_IMG,
+        github: 'https://github.com/vishrutss/Learning-JavaScript'
+    },
+    {
+        id: 6,
+        title: 'Vulgar tweet identification using Machine Learning',
+        img: PYTHON_IMG,
+        github: 'https://github.com/queoui/VulgarTweetIdentification'
+    },
+    {
+        id: 7,
+        title: 'Internet Relay Chat Project',
+        img: PYTHON_IMG,
+        github: 'https://github.com/master-of-none/IPProject'
+    },
+    {
+        id: 8,
+        title: 'Product Key Activation using Asymmetric Cryptography',
+        img: PYTHON_IMG,
+        github: 'https://github.com/vishrutss/Product_Key_Activation_Project'
+    },
+    {
+        id: 9,
+        title: 'Driver Drowsiness Detection program',
+        img: MATLAB_IMG,
+        github: 'https://github.com/vishrutss/Driver-Drowsiness-Detection'
+    }
+]
+
 const Projects = () => {
     return (
         <section id="projects">
             <h5>My Work</h5>
             <h2>Some of my Projects</h2>
             <div className="container project__container">
-                <article className="project__item">
-                    <div className="project__item-image">
-                        <img src={RUST_IMG} alt="" />
-                    </div>
-                    <h3>A Text Based Adventure Game written in RUST</h3>
-                    <a href="https://github.com/vishrutss/Text-based-adventure-game-in-Rust" className='btn'>Github</a>
-                </article>
-                <article className="project__item">
-                    <div className="project__item-image">
-                        <img src={CPP_IMG} alt="" />
-                    </div>
-                    <h3>An Amusement Park created using OpenGL</h3>
-                    <a href="https://github.com/vishrutss/Amusement_Park" className='btn'>Github</a>
-                </article>
-                <article className="project__item">
-                    <div className="project__item-image">
-                        <img src={CPP_IMG} alt="" />
-                    </div>
-                    <h3>Image Editor Program</h3>
-                    <a href="https://github.com/vishrutss/Image_Editor" className='btn'>Github</a>
-                </article>
-                <article className="project__item">
-                    <div className="project__item-image">
-                        <img src={PYTHON_IMG} alt="" />
-                    </div>
-                    <h3>Chess AI Project</h3>
-                    <a href="https://github.com/vishrutss/ChessProject" className='btn'>Github</a>
-                </article>
-                <article className="project__item">
-                    <div className="project__item-image">
-                        <img src={JS_IMG} alt="" />
-                    </div>
-                    <h3>A group of JavaScript Projects</h3>
-                    <a href="https://github.com/vishrutss/Learning-JavaScript" className='btn'>Github</a>
-                </article>
-                <article className="project__item">
-                    <div className="project__item-image">
-                        <img src={PYTHON_IMG} alt="" />
-                    </div>
-                    <h3>Vulgar tweet identification using Machine Learning</h3>
-                    <a href="https://github.com/queoui/VulgarTweetIdentification" className='btn'>Github</a>
-                </article>
-                <article className="project__item">
-                    <div className="project__item-image">
-                        <img src={PYTHON_IMG} alt="" />
-                    </div>
-                    <h3>Internet Relay Chat Project</h3>
-                    <a href="https://github.com/master-of-none/IPProject" className='btn'>Github</a>
-                </article>
-                <article className="project__item">
-                    <div className="project__item-image">
-                        <img src={PYTHON_IMG} alt="" />
-                    </div>
-                    <h3>Product Key Activation using Asymmetric Cryptography</h3>
-                    <a href="https://github.com/vishrutss/Product_Key_Activation_Project" className='btn'>Github</a>
-                </article>
-                <article className="project__item">
-                    <div className="project__item-image">
-                        <img src={MATLAB_IMG} alt="" />
-                    </div>
-                    <h3>Driver Drowsiness Detection program</h3>
-                    <a href="https://github.com/vishrutss/Driver-Drowsiness-Detection" className='btn'>Github</a>
-                </article>
+                {
+                    data.map(({ id, title, img, github }) => {
+                        return (
+                            <article key={id} className="project__item">
+                                <div className="project__item-image">
+                                    <img src={img} alt={title} />
+                                </div>
+                                <h3>{title}</h3>
+                                <a href={github} className='btn'>Github</a>
+                            </article>
+                        )
+                    })
+                }
             </div>
         </section>
     )
