773ad49713cc6f5f5a827fb7e842ecc3db206c99
vishrutss <vishrutss@gmail.com>
2023-07-25 16:45:24 +0530

    Updates

---
+++ precache-manifest.cf89ebc46e1a8bc3fda3ebeebbd91e86.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "bd4c00c03ff140730bba",
+    "url": "/react-portfolio-website/static/css/main.98e5bb31.chunk.css"
+  },
+  {
+    "revision": "bd4c00c03ff140730bba",
+    "url": "/react-portfolio-website/static/js/main.bd4c00c0.chunk.js"
+  },
+  {
+    "revision": "a3c43650dfa3fdd6de71",
+    "url": "/react-portfolio-website/static/js/1.a3c43650.chunk.js"
+  },
+  {
+    "revision": "79a76d0c048fe45d0a4f",
+    "url": "/react-portfolio-website/static/js/runtime~main.79a76d0c.js"
+  },
+  {
+    "revision": "b046cf7f06023b41f246305ebaf51df6",
+    "url": "/react-portfolio-website/static/media/python.b046cf7f.png"
+  },
+  {
+    "revision": "0c3764392574997f3e23543dd913b678",
+    "url": "/react-portfolio-website/static/media/C++.0c376439.png"
+  },
+  {
+    "revision": "a56bb1a3579494767a335fdcc53e3993",
+    "url": "/react-portfolio-website/static/media/Resume.a56bb1a3.pdf"
+  },
+  {
+    "revision": "5bb80844a2438c93b442b12a4aad622e",
+    "url": "/react-portfolio-website/static/media/profile.5bb80844.png"
+  },
+  {
+    "revision": "47c47e27ee04baadafde586a8c1d3a4c",
+    "url": "/react-portfolio-website/static/media/about.47c47e27.jpg"
+  },
+  {
+    "revision": "2b74252230ecc5c34f66ecd6381e55cb",
+    "url": "/react-portfolio-website/static/media/js.2b742522.png"
+  },
+  {
+    "revision": "b628a6e90563a82c4925f58fa2eabe89",
+    "url": "/react-portfolio-website/static/media/bg.b628a6e9.jpg"
+  },
+  {
+    "revision": "44dc34c4956ea32a4b2519bcea8bf2df",
+    "url": "/react-portfolio-website/index.html"
+  }
+];
