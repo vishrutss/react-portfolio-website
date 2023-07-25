94d4c9f0a99181cae8a3492bba9d248e8b310fc2
vishrutss <vishrutss@gmail.com>
2023-07-25 16:08:19 +0530

    Updated footer.jsx

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,9 +1,28 @@
 import React from 'react'
 import './footer.css'
+import { AiOutlineGithub } from 'react-icons/ai'
+import { AiFillLinkedin } from 'react-icons/ai'
 
 const Footer = () => {
     return (
-        <div>Footer</div>
+        <footer>
+            <a href="#header" className='footer__logo'>Vishrut Sharma</a>
+            <ul className="permalinks">
+                <li><a href="#header">Home</a></li>
+                <li><a href="#about">About</a></li>
+                <li><a href="#experience">Experience</a></li>
+                <li><a href="#skills">Skills</a></li>
+                <li><a href="#projects">Projects</a></li>
+                <li><a href="#contact">Contact</a></li>
+            </ul>
+            <div className="footer__socials">
+                <a href="https://github.com/vishrutss"><AiOutlineGithub /></a>
+                <a href="https://www.linkedin.com/in/vishrut-sharma-8703b0113/"><AiFillLinkedin /></a>
+            </div>
+            <div className="footer__copyright">
+                <small>&copy; Vishrut S Sharma. All rights reserved.</small>
+            </div>
+        </footer>
     )
 }
 
