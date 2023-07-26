ee9764fdf154287956321a8e7fa026219cd893c0
vishrutss <vishrutss@gmail.com>
2023-07-26 18:32:51 +0530

    removed whatsapp section in contact.jsx

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -2,7 +2,6 @@ import React, { useRef } from 'react'
 import './contact.css'
 import { AiOutlineMail } from 'react-icons/ai'
 import { AiFillLinkedin } from 'react-icons/ai'
-import { BsWhatsapp } from 'react-icons/bs'
 import emailjs from 'emailjs-com'
 
 // Initializing EmailJS environment variables
@@ -40,12 +39,6 @@ const Contact = () => {
                         <h4>LinkedIn</h4>
                         <h5>Vishrut Sharma</h5>
                         <a href="https://www.linkedin.com/in/vishrut-sharma-8703b0113/" target='_blank' rel="noopener noreferrer">Send a message</a>
-                    </article>
-                    <article className="contact__option">
-                        <BsWhatsapp className='contact__option-icon' />
-                        <h4>WhatsApp</h4>
-                        <h5>971-427-8341</h5>
-                        <a href="https://api.whatsapp.com/send?phone=+19714278341" target='_blank' rel="noopener noreferrer">Send a message</a>
                     </article>
                 </div>
                 <form ref={form} onSubmit={sendEmail}>
